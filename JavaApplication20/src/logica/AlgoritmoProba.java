16ff6d18a34d4d22c1273492768d4f0896b825cc
AmpiePablo <39846644+AmpiePablo@users.noreply.github.com>
2019-09-25 23:47:47 -0600

    version 3

--- JavaApplication20/src/logica/AlgoritmoProba.java
+++ JavaApplication20/src/logica/AlgoritmoProba.java
@@ -1,5 +1,8 @@
 package logica;
+import java.io.*;
+import java.security.*;
 import java.util.*;
+import javax.crypto.spec.SecretKeySpec;
 /**
  * laita
  * @author Pablo Ampie
@@ -7,4 +10,30 @@ import java.util.*;
 public class AlgoritmoProba {
     private ArrayList<Integer> numeros;
     private ArrayList<String> abecedario;
+    private static SecretKeySpec secretKey;
+
+
+/*Description:
+Dado el siguiente código para definir una llave de AES
+configurado com
+o AES/ECB/PKCS5Padding,
+private static SecretKeySpec secretKey;
+*/
+public static void setKey(String myKey) throws UnsupportedEncodingException  {
+  MessageDigest sha = null;
+  try {
+      byte[] key = myKey.getBytes("UTF-8");
+    sha = MessageDigest.getInstance("SHA-1");
+    key = sha.digest(key);
+    key = Arrays.copyOf(key, 16);
+    secretKey = new SecretKeySpec(key, "AES");
+  }
+  catch (NoSuchAlgorithmException e) {
+    e.printStackTrace();
+  }
+  catch (UnsupportedEncodingException e) {
+    e.printStackTrace();
+  }
+
+}
 }
