3616cf140749e148256fb74fc2077b2a1bb1959f
AmpiePablo <39846644+AmpiePablo@users.noreply.github.com>
2019-09-25 22:50:21 -0600

    version 2

--- JavaApplication20/src/logica/AlgoritmoProba.java
+++ JavaApplication20/src/logica/AlgoritmoProba.java
@@ -1,9 +1,10 @@
 package logica;
-
+import java.util.*;
 /**
- *
+ * laita
  * @author Pablo Ampie
  */
 public class AlgoritmoProba {
-    
+    private ArrayList<Integer> numeros;
+    private ArrayList<String> abecedario;
 }
